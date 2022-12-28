7491d35a057c4a536194199c1cf27703e0a43cd3
雾恋 <zhuhong317626@163.com>
2022-12-28 18:55:42 +0800

    first commit

---
+++ es/index.js
@@ -0,0 +1,11 @@
+// 编写加法：src/add.js
+
+var setadd = function setadd(a, b) {
+  return a + b;
+};
+
+var index = {
+  add: setadd
+};
+
+export { index as default };
---
+++ src/add.js
@@ -0,0 +1,7 @@
+// 编写加法：src/add.js
+
+const setadd = (a, b) => {
+    return a + b;
+};
+
+export default setadd;
